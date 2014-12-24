e660559dd436109287e23d2872446f187418c215
SONY <SONY@SONY_PC>
2014-12-25 00:42:14 +0100

    update help and some cosmetics changes

--- src/mtk/resizer/gui/ResizerPanel.java
+++ src/mtk/resizer/gui/ResizerPanel.java
@@ -1,13 +1,15 @@
 package mtk.resizer.gui;
 
+import static mtk.resizer.util.Util.CENT;
+
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 import mtk.resizer.ctrl.ResizerListener;
 import mtk.resizer.util.Util;
-import static mtk.resizer.util.Util.CENT;
 
 public class ResizerPanel extends JPanel {
 
@@ -41,38 +43,38 @@ public class ResizerPanel extends JPanel {
 		int dataWidth	= Math.round(frame.percents[2] * (totalWidth/((float) CENT)));
 		int fatWidth	= totalWidth - (sysWidth + cacheWidth + dataWidth);
 		int x = 0;
+		GradientPaint gp;
 
 		// ANDROID partition
 		x += 0;
-		g2.setColor(Color.CYAN);
-		g2.fill3DRect(x + shift, shift, sysWidth, height, true);
+		gp = new GradientPaint(x + shift, shift, Color.DARK_GRAY, shift, shift + height/2, Color.CYAN, true);
+		g2.setPaint(gp);
+		g2.fillRect(x + shift, shift, sysWidth, height);
 		g2.setColor(Color.BLACK);
-		g2.drawRect(x + shift, shift, sysWidth, height);
 		g2.drawString(Util.getPercent(frame.percents[0]) + "%", x + sysWidth/2, shift + 4 + height/2);
 
 		// CACHE partition
 		x += sysWidth;
-		g2.setColor(Color.YELLOW);
-		g2.fill3DRect(x + shift, shift, cacheWidth, height, true);
+		gp = new GradientPaint(x + shift, shift, Color.DARK_GRAY, x + shift, shift + height/2, Color.YELLOW, true);
+		g2.setPaint(gp);
+		g2.fillRect(x + shift, shift, cacheWidth, height);
 		g2.setColor(Color.BLACK);
-		g2.drawRect(x + shift, shift, cacheWidth, height);
 		g2.drawString(Util.getPercent(frame.percents[1]) + "%", x + cacheWidth/2, shift + 4 + height/2);
 
 		// USRDATA partition
 		x += cacheWidth;
-		g2.setColor(Color.BLUE);
-		g2.fill3DRect(x + shift, shift, dataWidth, height, true);
-		g2.setColor(Color.BLACK);
-		g2.drawRect(x + shift, shift, dataWidth, height);
+		gp = new GradientPaint(x + shift, shift, Color.DARK_GRAY, x + shift, shift + height/2, Color.BLUE, true);
+		g2.setPaint(gp);
+		g2.fillRect(x + shift, shift, dataWidth, height);
 		g2.setColor(Color.WHITE);
 		g2.drawString(Util.getPercent(frame.percents[2]) + "%", x + dataWidth/2, shift + 4 + height/2);
 
 		// FAT partition
 		x += dataWidth;
-		g2.setColor(Color.GREEN);
-		g2.fill3DRect(x + shift, shift, fatWidth, height, true);
+		gp = new GradientPaint(x + shift, shift, Color.DARK_GRAY, x + shift, shift + height/2, Color.GREEN, true);
+		g2.setPaint(gp);
+		g2.fillRect(x + shift, shift, fatWidth, height);
 		g2.setColor(Color.BLACK);
-		g2.drawRect(x + shift, shift, fatWidth, height);
 		g2.drawString(100 - (Util.getPercent(frame.percents[0]) + Util.getPercent(frame.percents[1]) + Util.getPercent(frame.percents[2])) + "%", x + fatWidth/2, shift + 4 + height/2);
 
 		frame.refreshSize();
